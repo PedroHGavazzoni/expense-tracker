8b3f8f691a9a4199f405c35f8ed238960f275b31
Pedro H. Gavazzoni <gavazzonipedroh@gmail.com>
2023-06-13 15:55:47 -0300

    Add files via upload

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+    { date: new Date(2023, 5, 29), category: 'food', title: 'McDonalds', value: 32.12},
+    { date: new Date(2023, 5, 15), category: 'food', title: 'Burger King', value: 28},
+    { date: new Date(2023, 5, 10), category: 'rent', title: 'Aluguel Apt', value: 2300},
+    { date: new Date(2023, 6, 13), category: 'salary', title: 'Salário', value: 4500},
+];
